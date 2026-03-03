18bd694ee0f0caefd41c44f0f44087befb094078
moyebin <midnight12315@gmail.com>
2026-03-04 02:56:22 +0900

    Add files via upload

---
+++ js/mainscript.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  
+  new Typed("#typed-text", {
+    strings: [
+      "무엇이든 물어보세요 :)",
+      "신입 웹퍼블리셔 Mo Yebin입니다.",
+      "포트폴리오를 확인해보세요!",
+    ],
+    typeSpeed: 50,
+    backSpeed: 30,
+    backDelay: 1500,
+    startDelay: 500,
+    loop: true,
+    showCursor: false,
+  });
+  
+});
+
+
+
